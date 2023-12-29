06cb634e5d7f2aa13d31eb344543f6afb8453c53
kyli <ldh71777@gmail.com>
2023-12-29 15:20:51 +0800

    chore: upgrade rspack 0.4.x

--- rspack.config.ts
+++ rspack.config.ts
@@ -1,13 +1,58 @@
 import { Configuration } from '@rspack/cli'
+import { rspack } from '@rspack/core'
+import ReactRefreshPlugin from '@rspack/plugin-react-refresh'
+
+const isDev = process.env.NODE_ENV === 'development'
 
 const config: Configuration = {
   entry: {
     main: './src/main.tsx',
   },
-  builtins: {
-    html: [
+  plugins: [
+    new rspack.HtmlRspackPlugin({
+      template: './index.html',
+    }),
+    isDev ? new ReactRefreshPlugin() : null,
+    new rspack.ProgressPlugin({}),
+  ],
+
+  module: {
+    rules: [
       {
-        template: './index.html',
+        test: /\.svg$/,
+        type: 'asset',
+      },
+      {
+        test: /\.(jsx?|tsx?)$/,
+        use: [
+          {
+            loader: 'builtin:swc-loader',
+            options: {
+              sourceMap: true,
+              jsc: {
+                parser: {
+                  syntax: 'typescript',
+                  tsx: true,
+                },
+                transform: {
+                  react: {
+                    runtime: 'automatic',
+                    development: isDev,
+                    refresh: isDev,
+                  },
+                },
+              },
+              env: {
+                targets: [
+                  'chrome >= 87',
+                  'edge >= 88',
+                  'firefox >= 78',
+                  'safari >= 14',
+                ],
+              },
+            },
+          },
+        ],
       },
     ],
   },
